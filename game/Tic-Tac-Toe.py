40053a55ee2d0e897ddc6e9fced810f7b83fcc10
Madhushree Warke <madhushreewarke24@gmail.com>
2026-02-14 19:08:46 +0530

    Update Tic-Tac-Toe.py

--- game/Tic-Tac-Toe.py
+++ game/Tic-Tac-Toe.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 def check_winner():
-  for combo in [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]:
+  for combo in [[0,1,2], [3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]:      //winning combinations
     if buttons[combo[0]]["text"] == buttons[combo[1]]["text"] == buttons[combo[2]]["text"] !="":
         buttons[combo[0]].config(bg="green")
         buttons[combo[1]].config(bg="green")
@@ -26,12 +26,13 @@ root.title("Tic-Tac-Toc")
 
 buttons = [tk.Button(root, text="" , font=("normal", 25),width=6,height=2,command=lambda i=i: button_click(i))for i in range(9)]
 
-for i, button in enumerate(buttons):
+for i, button in enumerate(buttons):        //3X3 grid pattern
   button.grid(row=i //3, column=i % 3)
 
 current_player = "x"
 winner = False
 label = tk.Label(root, text=f"Player {current_player}'s turn", font=("normal", 16))  
 label.grid(row=3, column=0, columnspan=3)
+
 
 root.mainloop()
